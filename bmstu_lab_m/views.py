9d328e8d72404e0f1d7636e75a463ab23572010d
Olga Khoven <olgahoven@gmail.com>
2023-10-01 22:35:24 +0300

    done

--- bmstu_lab_m/views.py
+++ bmstu_lab_m/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from datetime import date
 
 data ={'building': [
-    {'id':0, 'title':'Главное здание','img_url':'../static/images/gz.png','description':'2-я Бауманская ул., 5, стр. 4'},
-    {'id':1, 'title':'Учебно-лабораторный корпус','img_url':'../static/images/ulk_1.png','description':'Рубцовская наб., 2/18'},
-    {'id':2, 'title':'Энергомашиностроительный корпус','img_url':'../static/images/energo.jpg','description':'Лефортовская наб., 1'},
-    {'id':3, 'title':'Специальное машиностроение','img_url':'../static/images/sm.jpg','description':'Госпитальный пер., 10 '},
-    {'id':4, 'title':'Технологический корпус','img_url':'../static/images/ibm.jpg','description':'2-я Бауманская ул., 7 '}
+    {'id':0, 'title':'Главное здание','img_url':'../static/images/gz.png','description':'2-я Бауманская ул., 5, стр. 4', 'opening_hours': 'Режим работы: С 9:00 до 18:00'},
+    {'id':1, 'title':'Учебно-лабораторный корпус','img_url':'../static/images/ulk_1.png','description':'Рубцовская наб., 2/18','opening_hours': 'Режим работы: С 9:00 до 17:30'},
+    {'id':2, 'title':'Энергомашиностроительный корпус','img_url':'../static/images/energo.jpg','description':'Лефортовская наб., 1','opening_hours': 'Режим работы: С 9:00 до 17:30'},
+    {'id':3, 'title':'Специальное машиностроение','img_url':'../static/images/sm_1.png','description':'Госпитальный пер., 10 ','opening_hours': 'Режим работы: С 9:00 до 18:00'},
+    {'id':4, 'title':'Технологический корпус','img_url':'../static/images/ibm.jpg','description':'2-я Бауманская ул., 7 ','opening_hours': 'Режим работы: С 9:00 до 17:30'}
     ]}
 
 
@@ -15,9 +15,10 @@ def GetDetailedAboutPermit(request, id):
     return render(request, 'bmstu/permit.html', {
         'building': data_id
     })
+    
 
 def FindBuild(request):
-    build_name = request.GET.get('q')
+    build_name = request.GET.get('building') 
 
     if build_name:
         find = [item for item in data['building'] if build_name.lower() in item['title'].lower()]
